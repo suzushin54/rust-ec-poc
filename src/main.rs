fa9e1c861a9f962893ab45d17e6bb0f9b7d1b4b0
suzushin54 <mkm.snicr@gmail.com>
2024-12-17 23:51:40 +0900

    feat: logging with tracing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 mod router;
 mod handlers;
 
+use tracing::info;
+use tracing_subscriber;
+
 #[tokio::main]
 async fn main() {
+
+    tracing_subscriber::fmt()
+    .with_max_level(tracing::Level::INFO)
+    .init();
+
+    info!("Starting the server...");
+
     // build our application with a single route
     let app = router::create_router();
 
+    let address = "0.0.0.0:8080";
+    info!("Listening on {}", address);
+
     // run our app with hyper, listening globally on port 8080
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
