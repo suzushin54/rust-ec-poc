c5545c06a3294f922612ae924b46ca6afeb252d7
suzushin54 <mkm.snicr@gmail.com>
2024-12-20 23:46:28 +0900

    fix: create payment intent

--- src/handlers/payment.rs
+++ src/handlers/payment.rs
@@ -1,10 +1,12 @@
 use axum::{Router, Json, routing::post};
 use serde::{Deserialize, Serialize};
-use stripe::{Client, CreatePaymentIntent, Currency, PaymentIntent};
+use dotenv::dotenv;
+use std::env;
+use stripe::{Client, CreatePaymentIntent, Currency, PaymentIntent, PaymentMethodId};
 
 #[derive(Deserialize)]
 pub struct PaymentRequest {
-    amount: u64,
+    amount: i64,
 }
 
 #[derive(Serialize)]
@@ -15,44 +17,17 @@ pub struct PaymentResponse {
 }
 
 async fn handle_payment(Json(payload): Json<PaymentRequest>) -> Json<PaymentResponse> {
-    let secret_key = std::env::var("STRIPE_SECRET_KEY").expect("STRIPE_SECRET_KEY not set");
+    dotenv().ok();
+    let secret_key = env::var("STRIPE_SECRET_KEY").expect("STRIPE_SECRET_KEY not set");
     let client = Client::new(secret_key);
-    
-    let create_payment = CreatePaymentIntent {
-        amount: 200,
-        currency: Currency::JPY,
-        payment_method_types: Some(vec!["card".to_string()]),
-        application_fee_amount: None,
-        automatic_payment_methods: None,
-        capture_method: None,
-        confirm: None,
-        confirmation_method: None,
-        customer: None,
-        description: None,
-        error_on_requires_action: None,
-        expand: &[],
-        mandate: None,
-        mandate_data: None,
-        metadata: None,
-        off_session: None,
-        on_behalf_of: None,
-        payment_method: None,
-        payment_method_configuration: None,
-        payment_method_data: None,
-        payment_method_options: None,
-        radar_options: None,
-        receipt_email: None,
-        return_url: None,
-        setup_future_usage: None,
-        shipping: None,
-        statement_descriptor: None,
-        statement_descriptor_suffix: None,
-        transfer_data: None,
-        transfer_group: None,
-        use_stripe_sdk: None,
-    };
 
-    match PaymentIntent::create(&client, create_payment).await {
+    let mut params = CreatePaymentIntent::new(payload.amount, Currency::JPY);
+    params.payment_method_types = Some(vec!["card".to_string()]);
+    params.payment_method = Some("pm_card_visa".parse::<PaymentMethodId>().expect("Invalid PaymentMethodId"));
+    params.confirm = Some(true);
+    params.off_session = None; // Some(PaymentIntentOffSession::Exists(true));
+
+    match PaymentIntent::create(&client, params).await {
         Ok(payment_intent) => Json(PaymentResponse {
             success: true,
             message: "PaymentIntent created successfully".to_string(),
